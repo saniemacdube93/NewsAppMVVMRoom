8fa2380c8fc8ba6889f889502ad94db123e7f6d8
Busisani Mac Dube <saniemacdube93@gmail.com>
2022-01-21 23:31:43 +0200

    handling the breaking news response

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
@@ -1,18 +1,74 @@
 package com.androiddevs.mvvmnewsapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.androiddevs.mvvmnewsapp.R
 import com.androiddevs.mvvmnewsapp.ui.NewsActivity
+import com.androiddevs.mvvmnewsapp.ui.adapters.NewsAdapter
+import com.androiddevs.mvvmnewsapp.ui.util.Resource
 import com.androiddevs.mvvmnewsapp.ui.viewmodels.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
+import java.util.*
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
+    val TAG = "Breaking News Fragment"
+
+
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        viewModel.breakingNews.observe(viewLifecycleOwner , androidx.lifecycle.Observer { response ->
+            when(response){
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                    newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG , "An error occured: $message")
+                    }
+                }
+
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+
+
+
+
+            }
+        })
+
+    }
+
+    private fun hideProgressBar(){
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar(){
+    paginationProgressBar.visibility = View.VISIBLE
+    }
+
+
+    private fun setupRecyclerView(){
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/repository/NewsRepository.kt
@@ -1,9 +1,13 @@
 package com.androiddevs.mvvmnewsapp.ui.repository
 
+import com.androiddevs.mvvmnewsapp.ui.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.ui.db.ArticleDatabase
 
 
 //repository determines when to fetch data and from which source
-class NewsRepository(articleDatabase: ArticleDatabase) {
+class NewsRepository(
     val db: ArticleDatabase
+    ) {
+    suspend fun getBreakingNews(countryCode: String , pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNes(countryCode,pageNumber)
 }
